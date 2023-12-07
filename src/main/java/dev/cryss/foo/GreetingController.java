b26b286e995072e86ee8a20851fad1e7f7d2e0c4
Crystyan S. Santos <47446514+CrystyanSantos9@users.noreply.github.com>
2023-12-07 10:14:09 -0300

    incluindo path variabel, ainda não funcional

--- src/main/java/dev/cryss/foo/GreetingController.java
+++ src/main/java/dev/cryss/foo/GreetingController.java
@@ -1,19 +1,13 @@
 package dev.cryss.foo;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 public class GreetingController {
-    private static final String template = "Hello, %s!";
-    private final AtomicLong counter = new AtomicLong ();
-
-    @RequestMapping("/greeting")
-                             //greeting?name=value
-    public Greeting greeting(@RequestParam(name = "name", defaultValue = "World") String name){
-        return new Greeting (counter.incrementAndGet (), String.format (template, name));
+    @RequestMapping(value="/sum/numberOne/numberTwo", method = RequestMethod.GET)
+    public Double sum(@PathVariable("numberOne") String numberOne,@PathVariable("numberTwo") String numberTwo){
+        return 1D;
     }
 }
